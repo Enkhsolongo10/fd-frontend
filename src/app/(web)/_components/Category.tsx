972b3ff5b74bae3156fc5617ee43d362014a9c5d
Enkhsolongo10 <rrainbow1018@gmail.com>
2025-05-31 06:00:46 +0800

    update

--- src/app/(web)/_components/Category.tsx
+++
@@ -1,5 +0,0 @@
-export function UserCategory(){
-    return(
-        <div></div>
-    )
-};
--- src/app/(web)/_components/Container.tsx
+++
@@ -1,7 +0,0 @@
-export function Container(){
-    return(
-        <div className="flex justify-center">
-            <img className="h-[240px] w-[full] md:h-[500px] w-[100%]" src="container.png"/>
-        </div>
-    )
-};
